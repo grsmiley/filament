886533b85d9b217f3ad09296e345d96c0d968f6b
Greg Smiley <87918566+grsmiley@users.noreply.github.com>
2022-03-22 17:53:04 -0400

    Initial code

---
+++ filament.py
@@ -0,0 +1,106 @@
+from enum import Enum
+import inspect
+
+class BindingScope(Enum):
+        Transient = 1
+        Local = 2
+        Singleton = 3
+
+class BindingContext:
+    def __init__(self, singletons = None, locals = None, transients = None):
+        self._bindings = {} # (callable, binding type)
+   
+        if singletons:
+            for k,v in singletons.items():
+                self.singleton(k, v)
+
+        if locals:
+            for k,v in locals.items():
+                self.local(k, v)
+
+        if transients:
+            for k,v in transients.items():
+                self.transient(k, v)
+
+    def singleton(self, base, callable=None):
+        self._bind(base, callable, BindingScope.Singleton)
+
+    def local(self, base, callable=None):
+        self._bind(base, callable, BindingScope.Local)
+
+    def transient(self, base, callable=None):
+        self._bind(base, callable, BindingScope.Transient)
+
+    def _bind(self, base, callable, scope):
+        if not base:
+            # intentionally checking falsiness rather than None
+            raise ValueError(f"Base {base} cannot be falsy.")
+
+        if isinstance(base, str) and callable is None:
+            raise ValueError(f"Base {base} of type str must be explicitly mapped.")
+
+        # can't coalesce with (callable or base, scope) because 'data', {} should be valid
+        self._bindings[base] = (base if callable is None else callable, scope)
+
+    def get(self, target, default=None):
+        return self._bindings.get(target, default)
+
+class Injector:
+    def __init__(self, context:BindingContext=None, default_scope = BindingScope.Local):
+        self._context = context or BindingContext()
+        self._default_scope = default_scope
+        self._cache = {} # singletons
+
+    async def resolve(self, target, context:BindingContext=None):
+        context = context or BindingContext()
+
+        # ensure contexts both locally and at the injector level don't have collisions        
+        collision = context._bindings.keys() & self._context._bindings.keys()
+        assert not collision, f"Injector context and local context have duplicate bindings ({collision})."
+
+        cache = {} # local scope
+        
+        async def resolve_(target):
+            # search cache first
+            cached = cache.get(target)
+            if cached is None:
+                cached = self._cache.get(target)
+            if cached is not None:
+                return cached
+            
+            # get binding definition or default - local before global
+            # ok to coalesce here since target shouldn't be falsy
+            binding = context.get(target) or self._context.get(target)
+
+            if not binding and isinstance(target, str):
+                return None
+
+            concrete, scope = binding or (target, self._default_scope)
+
+            if callable(concrete):
+                args = {}
+                for param in inspect.signature(concrete).parameters.values():
+                    result = await resolve_(param.name)
+                    if result is None and param.annotation is not inspect.Parameter.empty:
+                        result = await resolve_(param.annotation)
+
+                    if result is not None:
+                        args[param.name] = result
+
+                result = concrete(**args)
+                if inspect.isawaitable(result):
+                    result = await result
+            else:
+                # not a callable
+                result = concrete
+
+            # set cache
+            match scope:
+                case BindingScope.Singleton:
+                    self._cache[target] = result
+                case BindingScope.Local:
+                    cache[target] = result
+
+            return result
+
+        return await resolve_(target)
---
+++ test_filament.py
@@ -0,0 +1,397 @@
+from argparse import ArgumentError
+from unittest import IsolatedAsyncioTestCase
+from filament import Injector, BindingContext, BindingScope
+
+class BindingContextCase(IsolatedAsyncioTestCase):
+    async def test_init(self):
+        bc = BindingContext()
+
+    async def test_init_singletons(self):
+        bc = BindingContext(singletons = {'x': 'y'})
+        result_map, result_scope = bc.get('x')
+        self.assertEqual(result_map, 'y')
+        self.assertEqual(result_scope, BindingScope.Singleton)
+
+    async def test_init_locals(self):
+        bc = BindingContext(locals = {'x': 'y'})
+        result_map, result_scope = bc.get('x')
+        self.assertEqual(result_map, 'y')
+        self.assertEqual(result_scope, BindingScope.Local)
+
+    async def test_init_transients(self):
+        bc = BindingContext(transients = {'x': 'y'})
+        result_map, result_scope = bc.get('x')
+        self.assertEqual(result_map, 'y')
+        self.assertEqual(result_scope, BindingScope.Transient)
+
+    async def test_str_must_be_explicit_singleton(self):
+        bc = BindingContext()
+        with self.assertRaises(ValueError):
+            bc.singleton('x')
+        bc.singleton('x', 'y')
+
+    async def test_str_must_be_explicit_local(self):
+        bc = BindingContext()
+        with self.assertRaises(ValueError):
+            bc.local('x')
+        bc.local('x', 'y')
+
+    async def test_str_must_be_explicit_transient(self):
+        bc = BindingContext()
+        with self.assertRaises(ValueError):
+            bc.transient('x')
+        bc.transient('x', 'y')
+
+    async def test_get(self):
+        bc = BindingContext(locals={'x':'y'})
+        not_found = object()
+        result = bc.get('y', not_found)
+        self.assertIs(result, not_found)
+        result = bc.get('x', not_found)
+        self.assertEqual(result, ('y', BindingScope.Local))
+
+    async def test_falsy_callable(self):
+        bc = BindingContext()
+        dict_ = dict()
+        bc.transient('test', dict_)
+        i = Injector(bc)
+        result = await i.resolve('test')
+        self.assertIs(result, dict_)
+
+class Resolve(IsolatedAsyncioTestCase):
+    async def test_resolve_callable(self):
+        i = Injector()
+        class A:
+            pass
+        result = await i.resolve(A)
+        self.assertIsInstance(result, A)
+
+    async def test_resolve_noncallable(self):
+        class A:
+            pass
+        bc = BindingContext(transients={A:'y'})
+        i = Injector(context=bc)
+        result = await i.resolve(A)
+        self.assertEqual(result, 'y')
+
+    async def test_resolve_string(self):
+        bc = BindingContext(transients={'x':'y'})
+        i = Injector(context=bc)
+        result = await i.resolve('x')
+        self.assertEqual(result, 'y')
+
+    async def test_resolve_awaitable(self):
+        i = Injector()
+        class A:
+            pass    
+        async def B(a:A):
+            return ('_', a)
+        _, result = await i.resolve(B)
+        self.assertIsInstance(result, A)
+
+    async def test_resolve_recursive(self):
+        i = Injector()
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+
+        class C:
+            def __init__(self, b:B):
+                self.b = b
+
+        result = await i.resolve(C)
+        self.assertIsInstance(result, C)
+        self.assertIsInstance(result.b, B)
+        self.assertIsInstance(result.b.a, A)
+
+    async def test_scope_transient(self):
+        class A:
+            pass
+        class B:
+            def __init__(self, a1:A, a2:A):
+                self.a1 = a1
+                self.a2 = a2
+        bc = BindingContext()
+        bc.transient(A)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a1, A)
+        self.assertIsInstance(result.a2, A)
+        self.assertIsNot(result.a1, result.a2)
+
+    async def test_scope_local(self):
+        class A:
+            pass
+        class B:
+            def __init__(self, a1:A, a2:A):
+                self.a1 = a1
+                self.a2 = a2
+        bc = BindingContext()
+        bc.local(A)
+        i = Injector(bc)
+        result1 = await i.resolve(B)
+        self.assertIsInstance(result1.a1, A)
+        self.assertIsInstance(result1.a2, A)
+        self.assertIs(result1.a1, result1.a2)
+
+        result2 = await i.resolve(B)
+        self.assertIsInstance(result2.a1, A)
+        self.assertIsInstance(result2.a2, A)
+        self.assertIs(result2.a1, result2.a2)
+
+        self.assertIsNot(result1.a1, result2.a1)
+        self.assertIsNot(result1.a2, result2.a2)
+
+    async def test_scope_singleton(self):
+        class A:
+            pass
+        class B:
+            def __init__(self, a1:A, a2:A):
+                self.a1 = a1
+                self.a2 = a2
+        bc = BindingContext()
+        bc.singleton(A)
+        i = Injector(bc)
+        result1 = await i.resolve(B)
+        self.assertIsInstance(result1.a1, A)
+        self.assertIsInstance(result1.a2, A)
+        self.assertIs(result1.a1, result1.a2)
+
+        result2 = await i.resolve(B)
+        self.assertIsInstance(result2.a1, A)
+        self.assertIsInstance(result2.a2, A)
+        self.assertIs(result2.a1, result2.a2)
+
+        self.assertIs(result1.a1, result2.a1)
+        self.assertIs(result1.a2, result2.a2)
+
+    async def test_default_mapping(self):
+        class A:
+            pass
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+        i = Injector()
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_implicit_mapping(self):
+        class A:
+            pass
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+        bc = BindingContext()
+        bc.local(A)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_explicit_mapping(self):
+        class A:
+            pass
+        class A_:
+            pass
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+        bc = BindingContext(locals={A:A_})
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a, A_)
+
+    async def test_resolution_order(self):
+        class A:
+            pass
+        class A_:
+            pass
+        class B:
+            def __init__(self, a:A_):
+                self.a = a
+
+        bc = BindingContext(locals={'a':A})
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_no_binding_for_positional(self):
+        class A:
+            def __init__(self, a):
+                self.a = a
+        i = Injector()
+        with self.assertRaises(TypeError):
+            result = await i.resolve(A)
+
+    async def test_default_scope(self):
+        class A:
+            pass
+        
+        class B:
+            def __init__(self, a1:A, a2:A):
+                self.a1 = a1
+                self.a2 = a2
+        
+        i = Injector(default_scope = BindingScope.Transient)
+        result = await i.resolve(B)
+        self.assertIsInstance(result.a1, A)
+        self.assertIsInstance(result.a2, A)
+        self.assertIsNot(result.a1, result.a2)
+        
+        i = Injector(default_scope = BindingScope.Local)
+        result1 = await i.resolve(B)
+        self.assertIsInstance(result1.a1, A)
+        self.assertIsInstance(result1.a2, A)
+        self.assertIs(result1.a1, result1.a2)
+        result2 = await i.resolve(B)
+        self.assertIsInstance(result2.a1, A)
+        self.assertIsInstance(result2.a2, A)
+        self.assertIsNot(result1.a1, result2.a1)
+
+
+        i = Injector(default_scope = BindingScope.Singleton)
+        result1 = await i.resolve(B)
+        self.assertIsInstance(result1.a1, A)
+        self.assertIsInstance(result1.a2, A)
+        self.assertIs(result1.a1, result1.a2)
+        result2 = await i.resolve(B)
+        self.assertIsInstance(result2.a1, A)
+        self.assertIsInstance(result2.a2, A)
+        self.assertIs(result1.a1, result2.a1)
+
+    async def test_duplicate_bindings(self):
+        bc1 = BindingContext(singletons={'x':'y'})
+        bc2 = BindingContext(locals={'x':'z'})
+        i = Injector(bc1)
+        with self.assertRaises(AssertionError):
+            result = await i.resolve('x', context=bc2)
+
+    async def test_falsy_cache(self):
+        dict_ = {}
+        class A:
+            def __init__(self, a1:'test', a2:'test'):
+                self.a1 = a1
+                self.a2 = a2
+
+        bc = BindingContext()
+        bc.local('test', lambda: {})
+        i = Injector(bc)
+        result = await i.resolve(A)
+        self.assertIs(result.a1, result.a2)
+
+    async def test_falsy_base_binding(self):
+        bc = BindingContext()
+        with self.assertRaises(ValueError):
+            bc.transient('', lambda:'test')
+
+    async def test_falsy_name_with_annotation(self):
+        dict_ = {}
+        class A:
+            pass
+
+        class B:
+            def __init__(self, test:A):
+                self.test = test
+        
+        bc = BindingContext()
+        bc.transient('test', lambda: dict_)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIs(result.test, dict_)
+
+class UseCases(IsolatedAsyncioTestCase):
+    async def test_string_injection(self):
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a):
+                self.a = a
+
+        bc = BindingContext()
+        bc.transient('a', A)
+        bc.transient('b', B)
+        i = Injector(bc)
+        result = await i.resolve('b')
+        self.assertIsInstance(result, B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_default_type_injection(self):
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+
+        i = Injector()
+        result = await i.resolve(B)
+        self.assertIsInstance(result, B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_implicit_type_injection(self):
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+
+        bc = BindingContext()
+        bc.transient(A)
+        bc.transient(B)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result, B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_explicit_type_injection(self):
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a:A):
+                self.a = a
+
+        bc = BindingContext()
+        bc.transient(A, A)
+        bc.transient(B, B)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result, B)
+        self.assertIsInstance(result.a, A)
+
+    async def test_mixed_injection(self):
+        class A:
+            pass
+
+        class B:
+            def __init__(self, a:A, my_value):
+                self.a = a
+                self.my_value = my_value
+
+        bc = BindingContext()
+        bc.transient(A)
+        bc.transient(B, B)
+        bc.transient('my_value', 7)
+        i = Injector(bc)
+        result = await i.resolve(B)
+        self.assertIsInstance(result, B)
+        self.assertIsInstance(result.a, A)
+        self.assertEqual(result.my_value, 7)
+
+    async def test_falsy_resolution(self):
+        dict_ = {}
+
+        class A:
+            def __init__(self, test):
+                self.test = test
+
+        bc = BindingContext()
+        bc.transient('test', dict_)
+        i = Injector(bc)
+        result = await i.resolve(A)
+        self.assertIs(result.test, dict_)
+
